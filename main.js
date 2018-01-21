38fe9bc9be72d00978b2b5b234c02525993253fd
Fabiano Paes <fabianopaes@gmail.com>
2018-01-21 12:41:43 -0200

    adding a function to listener the onclick event on submit form

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ userNavegateManager.onLoad = function(){
     var guest = storage.get(key);
     //it will mind if we already know the guest, if so... just storage the page
     if(guest){
-        console.log("user already exists, should storage page");
         storage.addPage(key, guest, getCurrentPage());
         return;
     }
@@ -23,7 +22,6 @@ userNavegateManager.onExit = function(){
         data.pages[data.pages.length-1] = lastPage;
         storage.set(key, data);
     }
-    console.log("Saaaaiu hahahha");
 }
 
 var getCurrentPage = function(){
@@ -41,8 +39,7 @@ storage.get = function(key) {
 }
 
 storage.start = function(){
-    return {"host" : window.location.host ,
-            "timestamp" : new Date(), 
+    return {"timestamp" : new Date(), 
             "contact": null, 
             "pages" : [ getCurrentPage() ]
      }
@@ -61,10 +58,18 @@ window.addEventListener("load", userNavegateManager.onLoad);
 //Starting listenning every single page leave
 window.addEventListener("beforeunload", userNavegateManager.onExit);
 
-
 /*
- FIXME put here the code to catch a click on some
- button and perform a request to the leads-api
+ Catching the click on the button and submit it
 */
+var onClickListener = function(formId, buttonId){
+    var email = document.getElementById(formId).innerText;
+    console.log(email);
+    document.getElementById(buttonId).
+        addEventListener("click", submit(email));
+}
+
+let submit = function(email){
+    console.log("Submitting..." + email);
+}
 
 
